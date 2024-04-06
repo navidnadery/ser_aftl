dc208e724c785b932a009d46a4437b21056bb94d
n.naderi <n.naderi@rahkar.co>
2024-04-06 21:48:24 +0330

    Add AM softmax loss function and related codes

---
+++ loss_functions.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class AngularPenaltySMLoss(nn.Module):
+
+    def __init__(self, in_features, out_features, loss_type='arcface', eps=1e-7, s=None, m=None):
+        '''
+        Angular Penalty Softmax Loss
+
+        Three 'loss_types' available: ['arcface', 'sphereface', 'cosface']
+        These losses are described in the following papers: 
+        
+        ArcFace: https://arxiv.org/abs/1801.07698
+        SphereFace: https://arxiv.org/abs/1704.08063
+        CosFace/Ad Margin: https://arxiv.org/abs/1801.05599
+
+        '''
+        super(AngularPenaltySMLoss, self).__init__()
+        loss_type = loss_type.lower()
+        assert loss_type in  ['arcface', 'sphereface', 'cosface']
+        if loss_type == 'arcface':
+            self.s = 64.0 if not s else s
+            self.m = 0.5 if not m else m
+        if loss_type == 'sphereface':
+            self.s = 64.0 if not s else s
+            self.m = 1.35 if not m else m
+        if loss_type == 'cosface':
+            self.s = 30.0 if not s else s
+            self.m = 0.4 if not m else m
+        self.loss_type = loss_type
+        self.in_features = in_features
+        self.out_features = out_features
+        self.fc = nn.Linear(in_features, out_features, bias=False)
+        self.eps = eps
+
+    def forward(self, x, labels, fromset = None, isset = None):
+        '''
+        input shape (N, in_features)
+        '''
+        assert len(x) == len(labels)
+        assert torch.min(labels) >= 0
+        assert torch.max(labels) < self.out_features
+        
+        for W in self.fc.parameters():
+            W = F.normalize(W, p=2, dim=1)
+
+        x = F.normalize(x, p=2, dim=1)
+
+        if fromset is None:
+            wf = self.fc(x)
+        else:
+            x_ = x[fromset[:]==isset,:]
+            if x_.shape[0] == 0:
+                return torch.tensor([0])
+            wf = self.fc(x_)
+
+        if self.loss_type == 'cosface':
+            if fromset is None:
+                numerator = self.s * (torch.diagonal(wf.transpose(0, 1)[labels]) - self.m)
+            else:
+                numerator = self.s * (torch.diagonal(wf.transpose(0, 1)[labels[fromset==isset]]) - self.m)
+        if self.loss_type == 'arcface':
+            numerator = self.s * torch.cos(torch.acos(torch.clamp(torch.diagonal(wf.transpose(0, 1)[labels]), -1.+self.eps, 1-self.eps)) + self.m)
+        if self.loss_type == 'sphereface':
+            numerator = self.s * torch.cos(self.m * torch.acos(torch.clamp(torch.diagonal(wf.transpose(0, 1)[labels]), -1.+self.eps, 1-self.eps)))
+
+        if fromset is None:
+            excl = torch.cat([torch.cat((wf[i, :y], wf[i, y+1:])).unsqueeze(0) for i, y in enumerate(labels)], dim=0)
+        else:
+            excl = torch.cat([torch.cat((wf[i, :y], wf[i, y+1:])).unsqueeze(0) for i, y in enumerate(labels[fromset==isset])], dim=0)
+        denominator = torch.exp(numerator) + torch.sum(torch.exp(self.s * excl), dim=1)
+        L = numerator - torch.log(denominator)
+        return -torch.mean(L), wf
+
+#voxceleb
+class LossFunction(nn.Module):
+    def __init__(self, nOut, nClasses, margin=0.3, scale=15, **kwargs):
+        super(LossFunction, self).__init__()
+
+        self.test_normalize = True
+        
+        self.m = margin
+        self.s = scale
+        self.in_feats = nOut
+        self.W = torch.nn.Parameter(torch.randn(nOut, nClasses), requires_grad=True)
+        self.ce = nn.CrossEntropyLoss()
+        nn.init.xavier_normal_(self.W, gain=1)
+
+    def forward(self, x, label=None):
+
+        assert x.size()[0] == label.size()[0]
+        assert x.size()[1] == self.in_feats
+
+        x_norm = torch.norm(x, p=2, dim=1, keepdim=True).clamp(min=1e-12)
+        x_norm = torch.div(x, x_norm)
+        w_norm = torch.norm(self.W, p=2, dim=0, keepdim=True).clamp(min=1e-12)
+        w_norm = torch.div(self.W, w_norm)
+        costh = torch.mm(x_norm, w_norm)
+        label_view = label.view(-1, 1)
+        if label_view.is_cuda: label_view = label_view.cpu()
+        delt_costh = torch.zeros(costh.size()).scatter_(1, label_view, self.m)
+        if x.is_cuda: delt_costh = delt_costh.cuda()
+        costh_m = costh - delt_costh
+        costh_m_s = self.s * costh_m
+        loss    = self.ce(costh_m_s, label)
+        return loss, costh
